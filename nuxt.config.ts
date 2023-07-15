36dc85828c0e7f1ddf68b51c2c03b80aea3279ee
David Stack <2013388+davidstackio@users.noreply.github.com>
2023-07-15 14:10:23 -0400

    Add vuefire to nuxt config w/ auth set to true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// Project config
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
 export default defineNuxtConfig({
   ssr: false,
+  modules: ["nuxt-vuefire"],
+  vuefire: {
+    config: firebaseConfig,
+    auth: true,
+  },
   devtools: { enabled: true },
 });
